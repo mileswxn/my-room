7494c7021ead745464c26a182395784ae9a4fb19
wangxunn <wangxunn@gmail.com>
2017-02-17 17:42:57 -0800

    added React and Webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
-/**
- * Created by moredeveloper on 2/16/17.
- */
+import React from 'react'
+import ReactDom from 'react-dom'
+import * as request from 'superagent'
+
+const app = document.getElementById('app')
+
+class HelloWorld extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      content: {__html: "Hello World!"}
+    }
+  }
+
+  componentWillMount() {
+    request.get('/public/first.html')
+      .then((res) => {
+        this.setState({
+          content: {
+            __html: res.text
+          }
+        })
+      })
+  }
+
+  render() {
+    return <div dangerouslySetInnerHTML={this.state.content}/>
+  }
+}
+
+ReactDom.render(<HelloWorld/>, app)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,46 @@
-/**
- * Created by moredeveloper on 2/16/17.
- */
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var publicPath = __dirname + '/public';
+var NODE_ENV = process.env.NODE_ENV || 'develop';
+
+
+module.exports = {
+  watch: NODE_ENV === 'develop',
+  entry: './src/app.js',
+  output: {
+    path: __dirname,
+    filename: 'public/main.js',
+    publicPath: '/public/'
+  },
+  devtool: '#sourcemap',
+  plugins: [
+    // new webpack.DefinePlugin(
+    //   {
+    //     'process.env': {
+    //       'NODE_ENV': JSON.stringify(NODE_ENV)
+    //     }
+    //   }),
+    new ExtractTextPlugin('public/app.css')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      },
+      {test: /\.(jpg|png|svg|ttf|woff2|woff|eot)(\?.*)?$/, loader: "file?name=tmp/assets/[name]-[hash].[ext]"},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract( "css?camelCase&modules&sourceMap&localIdentName=[local]--from--[path][name][ext]&root=" + publicPath + "!sass?sourceMap")}, //autoprefix
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("css?sourceMap&root=" + publicPath + "")} //autoprefix
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    port: 9000
+  }
+};
